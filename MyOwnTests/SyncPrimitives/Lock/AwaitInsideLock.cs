bd502eba8aaf88e7b0b95a024b53995dedc3c3ad
Issa Mansur <issa_mansur@mail.ru>
2025-02-06 10:30:26 +0200

    Fix example of `await inside lock`

--- MyOwnTests/SyncPrimitives/Lock/AwaitInsideLock.cs
+++ MyOwnTests/SyncPrimitives/Lock/AwaitInsideLock.cs
@@ -19,7 +19,7 @@ public class AwaitInsideLock
 
             Console.WriteLine("State before await 1 " + Environment.CurrentManagedThreadId);
             // Вставляем асинхронную операцию ИЛИ просто блокируем поток на время, чтобы увидеть разницу
-            // await Task.Delay(5000).ConfigureAwait(true);
+            await Task.Delay(5000).ConfigureAwait(true);
             // Thread.Sleep(5000);
 
             Console.WriteLine("State after await 1 " + Environment.CurrentManagedThreadId);
@@ -35,40 +35,14 @@ public class AwaitInsideLock
             }
         }
     }
-
-    public async Task AnotherMethodAsync()
-    {
-        bool lockTaken = false;
-        try
-        {
-            Console.WriteLine("State before lock 2 " + Environment.CurrentManagedThreadId);
-            Monitor.Enter(_lockObject, ref lockTaken);
-            Console.WriteLine("State after lock 2 " + Environment.CurrentManagedThreadId);
-            Console.WriteLine("AnotherMethodAsync вошёл в Monitor");
-            // Здесь можно добавить дополнительные операции
-        }
-        finally
-        {
-            if (lockTaken)
-            {
-                Console.WriteLine("State before unlock 2 " + Environment.CurrentManagedThreadId);
-                Monitor.Exit(_lockObject);
-                Console.WriteLine("State after unlock 2 " + Environment.CurrentManagedThreadId);
-            }
-        }
-    }
 }
 
-public static class WhyLockCannotContainsAwaitStarter
+public static class AwaitInsideLockStarter
 {
     public static async Task Start()
     {
         var example = new AwaitInsideLock();
 
-        // Запускаем два метода одновременно
-        var task1 = example.DoWorkAsync();
-        var task2 = example.AnotherMethodAsync();
-
-        await Task.WhenAll(task1, task2);
+        await example.DoWorkAsync();
     }
 }
